8420c4aeaa071b7b223f88b710bc569a3d9e38b8
Andrew Williams <qazwer8889@gmail.com>
2016-02-09 16:32:07 -0500

    Added the movie details as an item in the recycler view as well as added headers for the lists.

--- app/src/main/java/com/example/andy/popularmovies/DetailsActivityFragment.java
+++ app/src/main/java/com/example/andy/popularmovies/DetailsActivityFragment.java
@@ -16,9 +16,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.andy.popularmovies.model.ClipInfo;
@@ -28,7 +25,6 @@ import com.example.andy.popularmovies.model.Review;
 import com.example.andy.popularmovies.model.ReviewResults;
 import com.example.andy.popularmovies.service.MovieDatabaseHelper;
 import com.example.andy.popularmovies.service.MovieResultsServiceHelper;
-import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
@@ -47,17 +43,12 @@ public class DetailsActivityFragment extends Fragment implements Callback, Loade
     public static final String MOVIE_KEY = "movie";
     private static final String YOUTUBE_URI = "https://www.youtube.com/watch?v=";
     private static final int LOADER = 0;
+    private boolean checkedFavorites;
     private List<Review> reviews;
     private List<ClipInfo> clipInfoList;
     private RecyclerView mRecyclerView;
     private MovieDetailsAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private TextView mMovieTitle;
-    private TextView mRating;
-    private TextView mDateReleased;
-    private TextView mOverview;
-    private ImageView mPoster;
-    private Button mFavorite;
     private Movie movie;
 
     public DetailsActivityFragment() {
@@ -72,21 +63,10 @@ public class DetailsActivityFragment extends Fragment implements Callback, Loade
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_details, container, false);
         movie = Parcels.unwrap(getArguments().getParcelable(MOVIE_KEY));
-        mMovieTitle = (TextView) rootView.findViewById(R.id.movieTitleText);
-        mMovieTitle.setText(movie.getTitle());
-        mRating = (TextView) rootView.findViewById(R.id.rating);
-        mRating.setText(movie.getVote_average());
-        mDateReleased = (TextView) rootView.findViewById(R.id.dateReleasedText);
-        mDateReleased.setText(movie.getRelease_date());
-        mOverview = (TextView) rootView.findViewById(R.id.overviewText);
-        mOverview.setText(movie.getOverview());
-        mPoster = (ImageView) rootView.findViewById(R.id.posterImage);
-        Picasso.with(getActivity()).load(getActivity().getString(R.string.poster_url) + movie.getPoster_path()).into(mPoster);
-        mFavorite = (Button) rootView.findViewById(R.id.favoriteButton);
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new MovieDetailsAdapter(this);
+        mAdapter = new MovieDetailsAdapter(this.getActivity(), this, movie);
         mRecyclerView.setAdapter(mAdapter);
         getLoaderManager().initLoader(LOADER, null, this);
         MovieResultsServiceHelper.queryClipInfo(getActivity(), Integer.toString(movie.getId()), this);
@@ -121,45 +101,9 @@ public class DetailsActivityFragment extends Fragment implements Callback, Loade
                 cursor.close();
             }
         }
-        if (isFavorite) {
-            mFavorite.setText(R.string.unfavorite_button);
-            mFavorite.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    MovieDatabaseHelper movieDatabaseHelper = new MovieDatabaseHelper(getActivity());
-                    SQLiteDatabase db = movieDatabaseHelper.getWritableDatabase();
-                    db.delete(MovieSchema.TABLE_NAME, MovieSchema.COLUMN_NAME_MOVIE_ID + "=" + Integer.toString(movie.getId()), null);
-                    Toast.makeText(getActivity(), "Removed from favorites", Toast.LENGTH_SHORT).show();
-                    checkFavorites();
-                }
-            });
-        } else {
-            mFavorite.setText(R.string.favorite_button);
-            mFavorite.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    MovieDatabaseHelper movieDatabaseHelper = new MovieDatabaseHelper(getActivity());
-                    SQLiteDatabase db = movieDatabaseHelper.getWritableDatabase();
-                    ContentValues values = new ContentValues();
-                    values.put(MovieSchema.COLUMN_NAME_MOVIE_ID, movie.getId());
-                    values.put(MovieSchema.COLUMN_NAME_ADULT, movie.isAdult());
-                    values.put(MovieSchema.COLUMN_NAME_BACKDROP, movie.getBackdrop_path());
-                    values.put(MovieSchema.COLUMN_NAME_GENRE, Arrays.toString(movie.getGenre_ids()));
-                    values.put(MovieSchema.COLUMN_NAME_DATE, movie.getRelease_date());
-                    values.put(MovieSchema.COLUMN_NAME_LANGUAGE, movie.getOriginal_language());
-                    values.put(MovieSchema.COLUMN_NAME_ORIGINAL_TITLE, movie.getOriginal_title());
-                    values.put(MovieSchema.COLUMN_NAME_OVERVIEW, movie.getOverview());
-                    values.put(MovieSchema.COLUMN_NAME_POPULARITY, movie.getPopularity());
-                    values.put(MovieSchema.COLUMN_NAME_POSTER, movie.getPoster_path());
-                    values.put(MovieSchema.COLUMN_NAME_TITLE, movie.getTitle());
-                    values.put(MovieSchema.COLUMN_NAME_VIDEO, movie.isVideo());
-                    values.put(MovieSchema.COLUMN_NAME_VOTE_AVG, movie.getVote_average());
-                    values.put(MovieSchema.COLUMN_NAME_VOTE_COUNT, movie.getVote_count());
-                    db.insert(MovieSchema.TABLE_NAME, null, values);
-                    Toast.makeText(getActivity(), "Added to favorites", Toast.LENGTH_SHORT).show();
-                    checkFavorites();
-                }
-            });
-        }
-
+        mAdapter.setIsFavorite(isFavorite);
+        mAdapter.notifyDataSetChanged();
+        checkedFavorites = true;
     }
 
     @Override
@@ -199,5 +143,39 @@ public class DetailsActivityFragment extends Fragment implements Callback, Loade
     @Override
     public void ClipViewCardClick(int position) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(YOUTUBE_URI + clipInfoList.get(position).getKey())));
+    }
+
+    @Override
+    public void FavoriteButtonPressed(boolean isFavorite) {
+        if (checkedFavorites) {
+            if (isFavorite) {
+                MovieDatabaseHelper movieDatabaseHelper = new MovieDatabaseHelper(getActivity());
+                SQLiteDatabase db = movieDatabaseHelper.getWritableDatabase();
+                db.delete(MovieSchema.TABLE_NAME, MovieSchema.COLUMN_NAME_MOVIE_ID + "=" + Integer.toString(movie.getId()), null);
+                Toast.makeText(getActivity(), "Removed from favorites", Toast.LENGTH_SHORT).show();
+            } else {
+                MovieDatabaseHelper movieDatabaseHelper = new MovieDatabaseHelper(getActivity());
+                SQLiteDatabase db = movieDatabaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(MovieSchema.COLUMN_NAME_MOVIE_ID, movie.getId());
+                values.put(MovieSchema.COLUMN_NAME_ADULT, movie.isAdult());
+                values.put(MovieSchema.COLUMN_NAME_BACKDROP, movie.getBackdrop_path());
+                values.put(MovieSchema.COLUMN_NAME_GENRE, Arrays.toString(movie.getGenre_ids()));
+                values.put(MovieSchema.COLUMN_NAME_DATE, movie.getRelease_date());
+                values.put(MovieSchema.COLUMN_NAME_LANGUAGE, movie.getOriginal_language());
+                values.put(MovieSchema.COLUMN_NAME_ORIGINAL_TITLE, movie.getOriginal_title());
+                values.put(MovieSchema.COLUMN_NAME_OVERVIEW, movie.getOverview());
+                values.put(MovieSchema.COLUMN_NAME_POPULARITY, movie.getPopularity());
+                values.put(MovieSchema.COLUMN_NAME_POSTER, movie.getPoster_path());
+                values.put(MovieSchema.COLUMN_NAME_TITLE, movie.getTitle());
+                values.put(MovieSchema.COLUMN_NAME_VIDEO, movie.isVideo());
+                values.put(MovieSchema.COLUMN_NAME_VOTE_AVG, movie.getVote_average());
+                values.put(MovieSchema.COLUMN_NAME_VOTE_COUNT, movie.getVote_count());
+                db.insert(MovieSchema.TABLE_NAME, null, values);
+                Toast.makeText(getActivity(), "Added to favorites", Toast.LENGTH_SHORT).show();
+            }
+            checkedFavorites = false;
+            checkFavorites();
+        }
     }
 }
--- app/src/main/java/com/example/andy/popularmovies/MovieDetailsAdapter.java
+++ app/src/main/java/com/example/andy/popularmovies/MovieDetailsAdapter.java
@@ -1,15 +1,19 @@
 package com.example.andy.popularmovies;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.andy.popularmovies.model.ClipInfo;
+import com.example.andy.popularmovies.model.Movie;
 import com.example.andy.popularmovies.model.MovieExtra;
 import com.example.andy.popularmovies.model.Review;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,14 +23,22 @@ import java.util.List;
  */
 public class MovieDetailsAdapter extends RecyclerView.Adapter<MovieDetailsAdapter.ViewHolder> {
     private AdapterInterface adapterInterface;
+    private Context context;
     private int lengthClipInfo;
+    private boolean isFavorite;
+    private Movie movie;
     private List<MovieExtra> cards;
 
-    private static final int CLIP = 0;
-    private static final int REVIEW = 1;
+    private static final int DETAILS = 0;
+    private static final int CLIP_HEADER = 1;
+    private static final int CLIP = 2;
+    private static final int REVIEW_HEADER = 3;
+    private static final int REVIEW = 4;
 
     public interface AdapterInterface {
         public void ClipViewCardClick(int position);
+
+        public void FavoriteButtonPressed(boolean isFavorite);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -36,7 +48,42 @@ public class MovieDetailsAdapter extends RecyclerView.Adapter<MovieDetailsAdapte
         }
     }
 
-    public class ClipViewHolder extends ViewHolder implements View.OnClickListener {
+    public class DetailsViewHolder extends ViewHolder {
+        TextView movieTitle;
+        TextView rating;
+        TextView dateReleased;
+        TextView overview;
+        ImageView poster;
+        Button favorite;
+
+        public DetailsViewHolder(View itemView) {
+            super(itemView);
+            movieTitle = (TextView) itemView.findViewById(R.id.movieTitleText);
+            rating = (TextView) itemView.findViewById(R.id.rating);
+            dateReleased = (TextView) itemView.findViewById(R.id.dateReleasedText);
+            overview = (TextView) itemView.findViewById(R.id.overviewText);
+            poster = (ImageView) itemView.findViewById(R.id.posterImage);
+            Picasso.with(context).load(context.getString(R.string.poster_url) + movie.getPoster_path()).into(poster);
+            favorite = (Button) itemView.findViewById(R.id.favoriteButton);
+            favorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapterInterface.FavoriteButtonPressed(isFavorite);
+                }
+            });
+        }
+    }
+
+    public class HeaderViewHolder extends ViewHolder {
+        TextView listHeader;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+            this.listHeader = (TextView) itemView.findViewById(R.id.list_header);
+        }
+    }
+
+    public class ClipViewHolder extends ViewHolder {
         ImageView youTubeIcon;
         TextView videoName;
 
@@ -44,11 +91,12 @@ public class MovieDetailsAdapter extends RecyclerView.Adapter<MovieDetailsAdapte
             super(itemView);
             this.youTubeIcon = (ImageView) itemView.findViewById(R.id.youtube_icon);
             this.videoName = (TextView) itemView.findViewById(R.id.video_name);
-        }
-
-        @Override
-        public void onClick(View v) {
-            adapterInterface.ClipViewCardClick(getLayoutPosition());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapterInterface.ClipViewCardClick(getLayoutPosition());
+                }
+            });
         }
     }
 
@@ -63,48 +111,95 @@ public class MovieDetailsAdapter extends RecyclerView.Adapter<MovieDetailsAdapte
         }
     }
 
-    public MovieDetailsAdapter(AdapterInterface adapterInterface) {
+    public MovieDetailsAdapter(Context context, AdapterInterface adapterInterface, Movie movie) {
+        this.context = context;
         this.adapterInterface = adapterInterface;
+        this.movie = movie;
         cards = new ArrayList<MovieExtra>();
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view;
-        if (viewType == CLIP) {
-            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_video, viewGroup, false);
-            return new ClipViewHolder(view);
-        } else {
-            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_review, viewGroup, false);
-            return new ReviewViewHolder(view);
+        switch (viewType) {
+            case DETAILS:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_movie_details, viewGroup, false);
+                return new DetailsViewHolder(view);
+            case CLIP_HEADER:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_header, viewGroup, false);
+                return new HeaderViewHolder(view);
+            case CLIP:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_video, viewGroup, false);
+                return new ClipViewHolder(view);
+            case REVIEW_HEADER:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_header, viewGroup, false);
+                return new HeaderViewHolder(view);
+            case REVIEW:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_review, viewGroup, false);
+                return new ReviewViewHolder(view);
+            default:
+                return null;
         }
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        if (position < lengthClipInfo) {
-            ClipInfo info = (ClipInfo) cards.get(position);
-            ClipViewHolder clipHolder = (ClipViewHolder) holder;
-            clipHolder.videoName.setText(info.getName());
-        } else {
-            Review review = (Review) cards.get(position);
-            ReviewViewHolder reviewHolder = (ReviewViewHolder) holder;
-            reviewHolder.author.setText(review.getAuthor());
-            reviewHolder.content.setText(review.getContent());
+        switch (getItemViewType(position)) {
+            case DETAILS:
+                DetailsViewHolder detailsViewHolder = (DetailsViewHolder) holder;
+                detailsViewHolder.movieTitle.setText(movie.getTitle());
+                detailsViewHolder.rating.setText(movie.getVote_average());
+                detailsViewHolder.dateReleased.setText(movie.getRelease_date());
+                detailsViewHolder.overview.setText(movie.getOverview());
+                Picasso.with(context).load(context.getString(R.string.poster_url) + movie.getPoster_path()).into(detailsViewHolder.poster);
+                if (isFavorite)
+                    detailsViewHolder.favorite.setText(R.string.unfavorite_button);
+                else
+                    detailsViewHolder.favorite.setText(R.string.favorite_button);
+                break;
+            case CLIP_HEADER:
+                HeaderViewHolder clipHeader = (HeaderViewHolder) holder;
+                clipHeader.listHeader.setText(R.string.video_header);
+                break;
+            case CLIP:
+                ClipInfo info = (ClipInfo) cards.get(position - CLIP);
+                ClipViewHolder clipHolder = (ClipViewHolder) holder;
+                clipHolder.videoName.setText(info.getName());
+                break;
+            case REVIEW_HEADER:
+                HeaderViewHolder reviewHeader = (HeaderViewHolder) holder;
+                reviewHeader.listHeader.setText(R.string.review_header);
+                break;
+            case REVIEW:
+                Review review = (Review) cards.get(position - REVIEW_HEADER);
+                ReviewViewHolder reviewHolder = (ReviewViewHolder) holder;
+                reviewHolder.author.setText(review.getAuthor());
+                reviewHolder.content.setText(review.getContent());
+                break;
         }
     }
 
     @Override
     public int getItemViewType(int position) {
-        if (position < lengthClipInfo)
+        if (position == DETAILS)
+            return DETAILS;
+        else if (position == CLIP_HEADER)
+            return CLIP_HEADER;
+        else if (position < lengthClipInfo + CLIP)
             return CLIP;
+        else if (position == lengthClipInfo + CLIP)
+            return REVIEW_HEADER;
         else
             return REVIEW;
+
     }
 
     @Override
     public int getItemCount() {
-        return cards.size();
+        if (cards.isEmpty())
+            return 1;
+        else
+            return cards.size() + 3;
     }
 
     public void addClipInfoList(List<ClipInfo> clipInfoList) {
@@ -114,5 +209,13 @@ public class MovieDetailsAdapter extends RecyclerView.Adapter<MovieDetailsAdapte
 
     public void addReviews(List<Review> reviews) {
         cards.addAll(reviews);
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
     }
 }
